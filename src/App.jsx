e4d53b8343c7a27c899ba63728ee547465d25ff3
Firman Hidayat <fhidayat400@gmail.com>
2022-12-31 12:46:03 +0700

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const navigation = [
 ];
 function App() {
     return (
-        <div className='max-w-7xl'>
+        <div className='max-w-7xl font-sans'>
             <Popover>
                 <section className='relatives'>
                     <nav className='flex items-center justify-between xl:justify-start'>
@@ -27,27 +27,28 @@ function App() {
                         <div className='md:flex w-full lg:pl-10 lg:justify-between justify-end items-center space-x-8 hidden'>
                             <ul className='flex flex-row justify-between'>
                                 {navigation.map((item) => (
-                                    <li className='font-medium px-3'>
+                                    <li className='font-semibold px-3 text-amber-600 hover:text-amber-500'>
                                         {item.name}
                                     </li>
                                 ))}
                             </ul>
-                            <button className='bg-orange-600 px-3 py-2 rounded-lg text-white'>
+                            <button className='bg-orange-600 px-3 py-2 rounded-lg text-white font-semibold hover:bg-orange-500'>
                                 Get In Touch
                             </button>
                         </div>
-                        <Transition 
-                        as={Fragment}
-                        enter='duration-150 ease-out'
-                        enterForm='opacity-0 scale-95'
-                        enterTo='opacity-100 scale-100'
-                        leave='duration-100 ease-in'
-                        leaveFrom='opacity-100 scale-100'
-                        leavetTo='opacity-0 scale-95'
+                        <Transition
+                            as={Fragment}
+                            enter='duration-150 ease-out'
+                            enterFrom='opacity-0 scale-95'
+                            enterTo='opacity-100 scale-100'
+                            leave='duration-100 ease-in'
+                            leaveFrom='opacity-100 scale-100'
+                            leaveTo='opacity-0 scale-95'
                         >
-                            <Popover.Panel focus className='md:hidden'> 
-                                Makan
-                            </Popover.Panel>
+                            <Popover.Panel
+                                focus
+                                className='absolute inset-0 top-0 z-10 origin-top-right transform p-2 transition'
+                            ></Popover.Panel>
                         </Transition>
                     </nav>
                 </section>
