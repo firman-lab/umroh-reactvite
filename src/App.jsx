2c66be87be55f64506135c56c3bdc045189fcc00
Firman Hidayat <fhidayat400@gmail.com>
2023-01-03 13:57:47 +0700

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from './components/Navbar';
 
 function App() {
     return (
-        <div className='max-w-7xl font-sans'>
+        <div className='font-inter'>
             <Navbar />
             <Header />
         </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,22 +2,30 @@ import React from 'react';
 
 function Header() {
     return (
-        <main className='relative px-6 pt-32 lg:px-8'>
-            <div className='mx-auto'>
-                <div className='hidden sm:mb-8 sm:flex sm:justify-center'>
-                    <div className='relative px-3 py-1 ring-1 ring-orange-500/20 hover:ring-orange-500/40 rounded-3xl'>
-                        Tentukan Keberangkatan anda sekarang.{' '}
-                        <span className='text-orange-600 font-semibold'>
-                            <a href='#'>
-                                Atur <span aria-hidden='true'>&rarr;</span>
-                            </a>
-                        </span>
+        <main className="w-full relative px-6 pt-16 lg:px-8 bg-[url('src/assets/images/bgs.png')] bg-cover pb-12">
+            <div className='flex justify-center items-center pt-20'>
+                <div className='flex-1'></div>
+                <div className='flex-1 items-start'>
+                    <div className='hidden sm:mb-8 sm:flex sm:justify-center'>
+                        <div className='relative text-white px-3 py-1 ring-1 ring-gray-400 hover:ring-gray-300 rounded-3xl'>
+                            Tentukan Keberangkatan anda sekarang.{' '}
+                            <span className='text-orange-600 shadow-white font-semibold'>
+                                <a href='#'>
+                                    Atur <span aria-hidden='true'>&rarr;</span>
+                                </a>
+                            </span>
+                        </div>
                     </div>
                     <div>
-                        <h1 className='text-4xl font-bold tracking-tight sm:text-center sm:text-6xl'>Allah Akan permudah jalanmu</h1>
+                        <h1 className='flex items-start justify-start font-bold text-white tracking-tight sm:text-4xl'>
+                            Wujudkan Perjalanan Ibadah Umroh
+                        </h1>
                     </div>
                 </div>
             </div>
+            {/* <div className='absolute inset-x-0 top-1 -z-10 transform-gpu overflow-hidden bg-fixed'>
+                <img src='src/assets/images/bgs.png' alt='bg' />
+            </div> */}
         </main>
     );
 }
