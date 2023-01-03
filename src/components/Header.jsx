9db935ff7408cb1ad394ca3e8f9f98e8ac3c26c2
firman-lab <fhidayat400@gmail.com>
2023-01-03 10:19:09 +0700

    change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,23 @@ import React from 'react';
 
 function Header() {
     return (
-        <section className='w-full'>
-            <div>
-                <img src='src/assets/images/header.png' alt='header-img' />
+        <main className='relative px-6 pt-32 lg:px-8'>
+            <div className='mx-auto'>
+                <div className='hidden sm:mb-8 sm:flex sm:justify-center'>
+                    <div className='relative px-3 py-1 ring-1 ring-orange-500/20 hover:ring-orange-500/40 rounded-3xl'>
+                        Tentukan Keberangkatan anda sekarang.{' '}
+                        <span className='text-orange-600 font-semibold'>
+                            <a href='#'>
+                                Atur <span aria-hidden='true'>&rarr;</span>
+                            </a>
+                        </span>
+                    </div>
+                    <div>
+                        <h1 className='text-4xl font-bold tracking-tight sm:text-center sm:text-6xl'>Allah Akan permudah jalanmu</h1>
+                    </div>
+                </div>
             </div>
-        </section>
+        </main>
     );
 }
 
