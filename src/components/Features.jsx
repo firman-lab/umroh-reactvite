a7279da715d55b5c198861d5e48c9cda5556f7f4
firman-lab <fhidayat400@gmail.com>
2023-01-05 07:27:08 +0700

    changes

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -3,35 +3,35 @@ import React from 'react';
 function Features(props) {
     return (
         <section className='max-w-7xl pt-12 pb-12 lg:mx-auto px-12'>
-            <div className='flex flex-col lg:flex-row justify-between'>
+            <div className='flex flex-auto lg:flex-row justify-between'>
                 <img
-                    className='rounded-3xl object-cover h-80 w-auto'
+                    className='rounded-3xl object-cover h-80 md:w-full w-2/3'
                     src='src/assets/images/hotels.jpg'
                     alt='hotels'
                 />
-                <div className='w-full text-left md:pl-2 lg:pl-8'>
+                <div className='w-full text-left ml-4 md:pl-2 lg:pl-8'>
                     <p className='text-orange-500 font-medium pt-2'>
                         Exclusive Hotels
                     </p>
-                    <h3 className='pt-2 text-4xl font-bold'>
+                    <h3 className='pt-1 text-3xl font-bold'>
                         Hotel Full Kenyamanan
                     </h3>
-                    <p className='text-gray-500 pt-4 space-y-4'>
+                    <p className='text-gray-400 pt-3 text-sm'>
                         Hotel pilihan terbaik untuk menunjang jamaah
-                        beristirahat <br /> setelah melaksanakan serangkaian
+                        beristirahat setelah melaksanakan serangkaian
                         ibadah Umroh. View langsung ke Mekkah -
                     </p>
-                    <div className='flex flex-auto justify-evenly pt-4'>
+                    <div className='flex flex-auto justify-evenly pt-8'>
                         <div className='p-2'>
                             <img
                                 className='h-10'
                                 src='src/assets/images/money.png'
                                 alt='money'
                             />
-                            <h5 className='font-semibold text-lg pt-2'>
+                            <h5 className='font-semibold text-base pt-3'>
                                 Tanpa Tambahan Biaya
                             </h5>
-                            <p>
+                            <p className='text-gray-400 text-sm pt-1'>
                                 Anda hanya perlu memilih, tanpa ada biaya
                                 tambahan.
                             </p>
@@ -42,10 +42,10 @@ function Features(props) {
                                 src='src/assets/images/custom.png'
                                 alt='money'
                             />
-                            <h5 className='font-semibold text-lg pt-2'>
+                            <h5 className='font-semibold text-base pt-3'>
                                 Custom Kamar
                             </h5>
-                            <p>
+                            <p className='text-gray-400 text-sm pt-1'>
                                 Bisa custom lokasi kamar yang diinginkan sesuai
                                 pilihan anda.
                             </p>
